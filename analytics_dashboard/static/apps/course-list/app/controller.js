f8da26cb846e349e67515f3aa5f21677d0e978a5
Dennis Jen <djen@edx.org>
2017-08-03 09:22:59 -0400

    initial page shows loading spinner

--- analytics_dashboard/static/apps/course-list/app/controller.js
+++ analytics_dashboard/static/apps/course-list/app/controller.js
@@ -6,11 +6,14 @@
  * - CourseListCollection: A `CourseListCollection` instance.
  * - rootView: A `CourseListRootView` instance.
  */
+import _ from 'underscore';
 import Backbone from 'backbone';
 import Marionette from 'marionette';
 import NProgress from 'nprogress';
 
 import CourseListView from 'course-list/list/views/course-list';
+import LoadingTemplate from 'components/loading/templates/plain-loading.underscore';
+import LoadingView from 'components/loading/views/loading-view';
 
 export default class CourseListController extends Marionette.Object {
   constructor(options) {
@@ -50,48 +53,34 @@ export default class CourseListController extends Marionette.Object {
       trackingModel: this.options.trackingModel,
       filteringEnabled: this.options.filteringEnabled,
     });
+
     const collection = this.options.courseListCollection;
-    let currentPage;
-    let table;
 
     try {
       collection.setStateFromQueryString(queryString);
-      this.options.rootView.showChildView('main', listView);
-      if (collection.isStale) {
-        // There was a querystring sort parameter that was different from the current collection
-        // state, so we have to sort and/or search the table.
+      if (collection.isStale || collection.getResultCount() === 0) {
+        const loadingView = new LoadingView({
+          model: collection,
+          template: _.template(LoadingTemplate),
+          successView: listView,
+        });
+        this.options.rootView.showChildView('main', loadingView);
 
-        // We don't just do collection.fullCollection.sort() here because we've attached custom
-        // sortValue options to the columns via Backgrid to handle null values and we must call the
-        // sort function on the Backgrid table object for those custom sortValues to have an effect.
-        // Also, for some unknown reason, the Backgrid sort overwrites the currentPage, so we will
-        // save and
-        // restore the currentPage after the sort completes.
-        currentPage = collection.state.currentPage;
-
-        if (collection.getSearchString() !== '') {
-          listView.getRegion('controls').currentView.getRegion('search').currentView.search();
+        let fetch = collection.fetch({reset: true});
+        if(fetch) {
+          fetch.complete(function(response){
+            if (response && response.status === 404) {
+              collection.reset();
+            }
+          });
         }
-
-        table = listView.getRegion('results').currentView.getRegion('main').currentView.table;
-
-        // `table` will be undefined if the search resulted in an error or no results alert instead
-        // of a table of results.
-        if (table !== undefined) {
-          table.currentView.sort(
-            collection.state.sortKey,
-            collection.state.order === 1 ? 'descending' : 'ascending',
-          );
-        }
-
-        collection.setPage(currentPage);
-
-        collection.isStale = false;
+      } else {
+        this.options.rootView.showChildView('main', listView);
       }
     } catch (e) {
       // These JS errors occur when trying to parse invalid URL parameters
       // FIXME: they also catch a whole lot of other kinds of errors where the alert message doesn't
-      // make much sense.
+      // make much sense
       if (e instanceof RangeError || e instanceof TypeError) {
         this.options.rootView.showAlert(
           'error',
